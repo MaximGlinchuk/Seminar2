f532299f42c362d1213b6ef91076fd3629306d73
Maxim Glinchuk <glinchuk.maxim@yandex.ru>
2023-02-15 20:33:58 +0400

    task 10 complete

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine($"Число {number} - не трехзначное");
+    return;
+}
+Console.WriteLine("Второе число = " + ((number - ((number / 100) * 100))/10));
+
