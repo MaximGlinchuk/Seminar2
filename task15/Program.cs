f0013f7d161021e8f748a452ef2255c28f3a5f23
Maxim Glinchuk <glinchuk.maxim@yandex.ru>
2023-02-15 21:15:49 +0400

    task15 complete

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите день недели: ");
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+if (DayOfWeek >= 1 && DayOfWeek <= 5)
+{
+    Console.WriteLine("Сегодня будний день! :(");
+}
+else if (DayOfWeek >= 6 && DayOfWeek <= 7)
+{
+    Console.WriteLine("Сегодня выходной день! :)");
+}
+else
+Console.WriteLine("Такого дня недели не существует!");
