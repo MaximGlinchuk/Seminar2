3f3d2007a0429f03198d078bb20fe38dcfac1fde
Maxim Glinchuk <glinchuk.maxim@yandex.ru>
2023-02-15 20:53:15 +0400

    task13 complete

---
+++ task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string StrNumber = number.ToString();
+if (number < 100)
+{
+    Console.WriteLine("В данном числе нет третьей цифры");
+}
+else;
+Console.WriteLine("Третье число: " + StrNumber[2]);
+Console.WriteLine("Третье число с конца: " + StrNumber[^3]);
